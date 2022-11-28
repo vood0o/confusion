4f36092bb0abef88771dcc37cb3e3966062b9192
Voicu Dordea <d0rdea2@gmail.com>
2022-11-28 10:24:54 +0200

    Data binding

---
+++ src/app/dishdetail/dishdetail.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Dish } from '../shared/dish';
+
+@Component({
+  selector: 'app-dishdetail',
+  templateUrl: './dishdetail.component.html',
+  styleUrls: ['./dishdetail.component.scss']
+})
+export class DishdetailComponent {
+
+  @Input()
+  dish: Dish | undefined;
+}
